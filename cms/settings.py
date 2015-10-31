516b785c399920338a871e21f8317453536b76b4
Dave Smith <davesmithdev@gmail.com>
2015-10-31 19:04:33 +0530

    Added index.html / Bower, etc.

--- cms/settings.py
+++ cms/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'web',
     'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -102,3 +103,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+#STATIC_ROOT = 'staticfiles'
+
+LOGIN_URL = '/login'
--- cms/urls.py
+++ cms/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+from web import views as web_views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', auth_views.login, {'extra_context': {'next': '/'}}),
+    url(r'^logout/$', auth_views.logout),
+    url(r'^$',web_views.index)
 ]
