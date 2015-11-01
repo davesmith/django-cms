769c58e00c97fc34de4f2a2914de504fd2f30785
jignesh2290 <parmarjignesh90@gmail.com>
2015-11-01 21:26:16 +0530

    Mould table updated

--- static/javascripts/mould/controllers/mould.controller.js
+++ static/javascripts/mould/controllers/mould.controller.js
@@ -20,7 +20,36 @@
     function activate(){
        Mould.listMoulds().then(function (data) {
         $scope.rowCollection = data.data;
+        //copy the references (you could clone ie angular.copy but then have to go through a dirty checking for the matches)
+        $scope.displayedCollection = [].concat($scope.rowCollection);
+
       });
+    }
+
+
+    //add to the real data holder
+    $scope.addItem = function addItem() {
+        $scope.rowCollection.push({});
+        // call the db to update the mould list
+    };
+
+     //add to the real data holder
+    $scope.updateItem = function updateItem(row) {
+        var index = $scope.rowCollection.indexOf(row);
+        if (index !== -1) {
+            $scope.rowCollection.splice(index, 1, row);
+        }
+        // call the db to update the mould list
+    };
+
+    //remove to the real data holder
+    $scope.removeItem = function removeItem(row) {
+        var index = $scope.rowCollection.indexOf(row);
+        if (index !== -1) {
+            $scope.rowCollection.splice(index, 1);
+        }
+
+        // call db to remove item from mould list
     }
   }
 })();
